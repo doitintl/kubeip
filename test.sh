c0848bb525705174bc35837f98714241dd73049f
Vadim Solovey <vadim@doit-intl.com>
2018-06-18 21:27:43 +0300

    changing the name to kubeip

--- test.sh
+++ test.sh
@@ -10,20 +10,20 @@ fi
 
 NODES=`gcloud container node-pools describe default-pool --cluster $CLUSTER|grep initialNodeCount|awk '{print $2}'`
 NEW_NODES=$(($NODES + 1))
-gcloud compute addresses create kip-test-1 --region $REGION
-gcloud beta compute addresses update kip-test-1 --update-labels kip=reserved  --region us-central1
-IP1=`gcloud compute addresses describe kip-test-1 --region $REGION|grep address:|awk '{print $2}'`
-gcloud compute addresses create kip-test-2 --region $REGION
-gcloud beta compute addresses update kip-test-2 --update-labels kip=reserved  --region us-central1
-IP2=`gcloud compute addresses describe kip-test-2 --region $REGION|grep address:|awk '{print $2}'`
+gcloud compute addresses create kubeip-test-1 --region $REGION
+gcloud beta compute addresses update kubeip-test-1 --update-labels kubeip=reserved  --region us-central1
+IP1=`gcloud compute addresses describe kubeip-test-1 --region $REGION|grep address:|awk '{print $2}'`
+gcloud compute addresses create kubeip-test-2 --region $REGION
+gcloud beta compute addresses update kubeip-test-2 --update-labels kubeip=reserved  --region us-central1
+IP2=`gcloud compute addresses describe kubeip-test-2 --region $REGION|grep address:|awk '{print $2}'`
 gcloud beta container clusters resize $CLUSTER --node-pool default-pool --size $NEW_NODES --quiet
 
-STATUS1=`gcloud compute addresses describe kip-test-1 --region $REGION|grep status|awk '{print $2}'`
-STATUS2=`gcloud compute addresses describe kip-test-2 --region $REGION|grep status|awk '{print $2}'`
+STATUS1=`gcloud compute addresses describe kubeip-test-1 --region $REGION|grep status|awk '{print $2}'`
+STATUS2=`gcloud compute addresses describe kubeip-test-2 --region $REGION|grep status|awk '{print $2}'`
 echo 'expecting one IP IN_USE and one RESERVED'
 echo 'Results:'
 echo $STATUS1 '--' $STATUS2
 
 gcloud beta container clusters resize $CLUSTER --node-pool default-pool --size $NODES --quiet
-gcloud compute addresses delete kip-test-1 --region $REGION --quiet
-gcloud compute addresses delete kip-test-2 --region $REGION --quiet
+gcloud compute addresses delete kubeip-test-1 --region $REGION --quiet
+gcloud compute addresses delete kubeip-test-2 --region $REGION --quiet
