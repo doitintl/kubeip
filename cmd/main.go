1ac1b82b8f3e9affcc238480cd59e324c1c75053
Alexei Ledenev <alexei.led@gmail.com>
2023-09-26 09:08:49 +0300

    explore k8s node

--- cmd/main.go
+++ cmd/main.go
@@ -8,16 +8,28 @@ import (
 	"time"
 
 	"github.com/doitintl/kubeip/internal/config"
+	"github.com/doitintl/kubeip/internal/node"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
+type contextKey string
+
+const (
+	developModeKey contextKey = "develop-mode"
+)
+
 var (
-	version   string
-	buildDate string
-	gitCommit string
-	gitBranch string
+	version      string
+	buildDate    string
+	gitCommit    string
+	gitBranch    string
+	errEmptyPath = errors.New("empty path")
 )
 
 const (
@@ -54,6 +66,9 @@ func prepareLogger(level string, json bool) *logrus.Entry {
 		logger.SetFormatter(&logrus.JSONFormatter{})
 	}
 
+	// record the file name and line number of the log
+	logger.SetReportCaller(true)
+
 	log := logger.WithFields(logrus.Fields{
 		"version": version,
 	})
@@ -61,11 +76,36 @@ func prepareLogger(level string, json bool) *logrus.Entry {
 	return log
 }
 
-func run(ctx context.Context, log *logrus.Entry, _ config.Config) error {
-	log.Infof("assigning static public IP address to the node")
+func run(c context.Context, log *logrus.Entry, cfg config.Config) error {
+	ctx, cancel := context.WithCancel(c)
+	defer cancel()
+	// add debug mode to context
+	if cfg.DevelopMode {
+		ctx = context.WithValue(ctx, developModeKey, true)
+	}
+
+	log.WithField("develop-mode", cfg.DevelopMode).Infof("kubeip agent started")
+
+	restconfig, err := retrieveKubeConfig(log, cfg)
+	if err != nil {
+		return errors.Wrap(err, "retrieving kube config")
+	}
+
+	clientset, err := kubernetes.NewForConfig(restconfig)
+	if err != nil {
+		return errors.Wrap(err, "initializing kubernetes client")
+	}
+
+	explorer := node.NewExplorer(clientset)
+	n, err := explorer.GetNode(ctx)
+	if err != nil {
+		return errors.Wrap(err, "getting node")
+	}
+
+	log.Debug("node name: ", n.Name)
 
 	<-ctx.Done()
-	log.Info("release static public IP address from the node")
+	log.Infof("kubeip agent stopped")
 	return nil
 }
 
@@ -89,14 +129,14 @@ func main() {
 				Usage: "run agent",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:     "cluster-name",
-						Usage:    "Kubernetes cluster name (not needed if running in cluster)",
+						Name:     "node-name",
+						Usage:    "Kubernetes node name (not needed if running in node)",
 						EnvVars:  []string{"CLUSTER_NAME"},
 						Category: "Configuration",
 					},
 					&cli.PathFlag{
 						Name:     "kubeconfig",
-						Usage:    "path to Kubernetes configuration file (not needed if running in cluster)",
+						Usage:    "path to Kubernetes configuration file (not needed if running in node)",
 						EnvVars:  []string{"KUBECONFIG"},
 						Category: "Configuration",
 					},
@@ -153,4 +193,41 @@ func main() {
 	if err != nil {
 		logrus.Fatal(err)
 	}
+}
+
+func kubeConfigFromPath(kubepath string) (*rest.Config, error) {
+	if kubepath == "" {
+		return nil, errEmptyPath
+	}
+
+	data, err := os.ReadFile(kubepath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "reading kubeconfig at %s", kubepath)
+	}
+
+	cfg, err := clientcmd.RESTConfigFromKubeConfig(data)
+	if err != nil {
+		return nil, errors.Wrapf(err, "building rest config from kubeconfig at %s", kubepath)
+	}
+
+	return cfg, nil
+}
+
+func retrieveKubeConfig(log logrus.FieldLogger, cfg config.Config) (*rest.Config, error) {
+	kubeconfig, err := kubeConfigFromPath(cfg.KubeConfigPath)
+	if err != nil && !errors.Is(err, errEmptyPath) {
+		return nil, errors.Wrap(err, "retrieving kube config from path")
+	}
+
+	if kubeconfig != nil {
+		log.Debug("using kube config from env variables")
+		return kubeconfig, nil
+	}
+
+	inClusterConfig, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "retrieving in node kube config")
+	}
+	log.Debug("using in node kube config")
+	return inClusterConfig, nil
 }
--- internal/types/node.go
+++ internal/types/node.go
@@ -1,16 +1,21 @@
 package types
 
-type Address struct {
-	IP   string
-	Type string
-}
+import "net"
+
+type CloudProvider string
+
+const (
+	CloudProviderGCP   CloudProvider = "gcp"
+	CloudProviderAWS   CloudProvider = "aws"
+	CloudProviderAzure CloudProvider = "azure"
+)
 
 type Node struct {
-	Name      string
-	Cluster   string
-	Pool      string
-	Spot      bool
-	Region    string
-	Zone      string
-	Addresses []string
+	Name        string
+	Cloud       CloudProvider
+	Pool        string
+	Region      string
+	Zone        string
+	ExternalIPs []net.IP
+	InternalIPs []net.IP
 }
